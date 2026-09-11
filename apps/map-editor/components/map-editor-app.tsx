6afc555b46986e949e6a68d72838e58d402ffabc
rakarakaa775 <raka.rakaa775@gmail.com>
2026-09-11 20:06:29 +0700

    Move TypeScript nocheck directive to file header

--- apps/map-editor/components/map-editor-app.tsx
+++ apps/map-editor/components/map-editor-app.tsx
@@ -1,5 +1,5 @@
-"use client";
 // @ts-nocheck
+"use client";
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { EditorShell } from './editor-shell';
 import { MapBrowser } from './map-browser';
